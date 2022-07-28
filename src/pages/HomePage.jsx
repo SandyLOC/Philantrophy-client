f9bea1a5854a286711ae4aedb2bdf53972bd94ee
SandyLOC <sandrapl.18@gmail.com>
2022-07-28 08:39:31 -0700

    :sparkles: Navbar, and pages added.

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+import SwipeableTextMobileStepper from "../components/Carousel/SwipeableTextMobileStepper";
+
+function HomePage() {
+  return (
+    <div className="App">
+      <SwipeableTextMobileStepper />
+    </div>
+  );
+}
+
+export default HomePage;
